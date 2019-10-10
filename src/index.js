7287f206d1f698a2e33e170a3142463ffa61e689
AlreadyBored <shilaums@gmail.com>
2019-10-10 21:13:34 +0300

    feat: ugly implementation made all test passed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
+    const patternGlued = [];
 
+    bracketsConfig.forEach(patternChunk => patternGlued.push(patternChunk.join('')));
 
+    const cutStr = (str) => {
+        let buffStr = str;
 
+        for(let i = 0; i < patternGlued.length; i++) {
+            buffStr = buffStr.replace(patternGlued[i], '');
+        }
+
+        return str === buffStr ? buffStr : cutStr(buffStr);
+    }
+
+    const res = cutStr(str);
+
+    return res === '' ? true : false;
 }
