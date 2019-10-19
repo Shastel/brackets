5fa8ec677e421d153682ac5bf5f0aa1f6d964f8d
KotdenGit <kotden09@gmail.com>
2019-10-19 14:26:55 +0300

    init: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = [];
+	for(let i = 0; i<str.length; i++){
+	  	for(let k = 0; k<bracketsConfig.length; k++){
+	  		if (str[i] === bracketsConfig[k][0] && str[i] === bracketsConfig[k][1]) { // for duble brackets
+	  			if (array[array.length-1] === str[i]){
+	  					array.pop();
+	  			} else {
+	  				array.push(str[i]);
+	  			} 
+	  		}
+	  		else if (str[i] === bracketsConfig[k][0]) {
+	  			array.push(k+1);
+	  		}
+	  		else if (str[i] === bracketsConfig[k][1]){
+	  			if ((array[array.length-1] - (k+1)) === 0){
+	  				array.pop();
+	  			} 
+	  			else {
+	  				return false;
+	  			}
+	  		}
+	  	}
+	}
+	return array.length === 0 ? true : false;
 }
