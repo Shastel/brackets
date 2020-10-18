d4d95aa0689bccb813374a8bff0d61a66d975f44
AlekseyJukovskiy <messagefordreamer@gmail.com>
2020-10-18 20:18:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    while(str !== ''){
+      let k = 0;
+      for (let i of bracketsConfig) {
+          let brackets = i[0] + i[1];
+          if (str.includes(brackets)){
+              str = str.replace(brackets, '');
+              k += 1;
+          }
+      }
+      if (k === 0) break;
+    }
+    if (str === '') return true;
+    return false;
 }
