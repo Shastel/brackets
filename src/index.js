ba79b9b301cdde9c23917f37446a577c03377bfc
Yana Gurinovich <yana.gurinovich@instatsport.com>
2020-10-20 21:21:02 +0300

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length%2 !== 0){
+        return false;
+     }
+     while(str !== '') {
+       let n=0;
+    for(let i of bracketsConfig){
+     let items = i[0]+i[1];
+     if (str.includes(items)){
+     str = str.replace(items, '')
+     n += 1;
+     }
+     } 
+     if(n===0) break;
+    } 
+     if (str === ''){
+       return true;
+     } else {
+       return false;
+     }
+    }
+      
+
+
+
+
