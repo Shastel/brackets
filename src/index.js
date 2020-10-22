c20be2e78d1394532a5cda12160e3eaf0418ed19
Andrey Goncharenko <goncharenko.andry@yandex.by>
2020-10-22 20:11:16 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let string = '';
+  let count = 0;
+
+  while(str != '') {
+      count = 0;
+      for(let i of bracketsConfig) {
+          string = i[0] + i[1];
+          
+          if(str.includes(string)) {
+              str = str.replace(string, '');
+              count = 1;
+          }
+      }
+      if(count == 0) break;
+  }
+
+  if (str == '') {return true} 
+  return false;
 }
