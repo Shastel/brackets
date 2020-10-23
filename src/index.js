7bc9dd0ce8fe7500e47e7ef4198d42c0793a10da
Alexey-Vasilkov <alexey.a.vasilkov@gmail.com>
2020-10-23 21:59:43 +0300

    init: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let closeBrackets = "]})>|24678";
+  let openBrackets = "[{(<|13578";
+  let stack = [];
+
+  for(let bracket of str) {
+    let bracketsIndexOpen = openBrackets.indexOf(bracket);
+    let bracketsIndexClose = closeBrackets.indexOf(bracket);    
+    if(bracketsIndexClose !== -1) {      
+      if(stack.length === 0) {
+        if (bracketsIndexOpen === bracketsIndexClose) {
+          stack.push(bracketsIndexOpen);
+        } else {          
+          return false;
+        }        
+      } else {        
+        if (stack[stack.length - 1] !== bracketsIndexClose) {
+          stack.push(bracketsIndexOpen);
+        } else {
+          stack.pop();
+        }
+      }
+    } else {
+      if (bracketsIndexOpen !== -1) {        
+        stack.push(bracketsIndexOpen);
+      }
+    }    
+  }  
+  return stack.length === 0
 }
