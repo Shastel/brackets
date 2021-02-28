f7274a8dfbd95b4ea72d1cfd07d00ddf5c0c1abb
Artem <tema.shevchuk@gmail.com>
2021-02-28 23:38:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+const { test } = require("mocha");
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    configAsArrayOfStrings = [];
+    bracketsConfig.forEach(item => {
+        configAsArrayOfStrings.push(item[0] + item[1]);
+    });
+    while (str.length > 0) {
+        let testStr = str;
+        configAsArrayOfStrings.forEach(pattern => {
+            testStr = testStr.replace(pattern, '');
+        });
+        if (testStr === str) {
+            return false;
+        }
+        str = testStr;
+    }
+    return true;
 }
