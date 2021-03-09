9ebc7292925cb21bbf31becde540a794d585221b
tokanito <78441278+tokanito@users.noreply.github.com>
2021-03-09 20:01:46 +0400

    feat:
    adding file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var arr=[];
+  arr.push(str[0]);
+  var result;
+  
+  for (var i=1; i<str.length; i++){
+    for (var j=0; j<bracketsConfig.length; j++){
+        if (bracketsConfig[j].includes(arr[arr.length-1])){
+            var num = j;
+        
+      if ((bracketsConfig[num].includes(str[i])) && 
+      (bracketsConfig[num].includes(arr[arr.length-1]))){  
+         arr.pop();
+       }else {
+         arr.push(str[i]);
+       }
+       }
+      }
+    }
+    if (arr.length===0){
+        result=true;
+    }else {
+        result=false;
+    }
+    
+    return result;
 }
