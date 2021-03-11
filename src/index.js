b3c65bf5835919ac50c85c07afba7db1114e5b3e
igotis <igotis@ukr.net>
2021-03-11 20:25:48 +0200

    feat: add compare function to str - bracketsConfig

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  if(typeof str !== 'string')
+  return false;
+
+  let   length = bracketsConfig.length,
+        brackets = "",
+        compare = "",
+        count;
+  
+  while (count !== length) {
+    count = 0;
+    
+    for (i = 0; i < length; i++) {
+      brackets = bracketsConfig[i][0] + bracketsConfig[i][1];
+      compare = str.replace(brackets, "");
+      if (str === compare) {
+        count++;
+      }
+      str = compare;
+    }
+  }
+   
+  if (str.length === 0) {
+      return true; 
+    } else {
+        return false;
+     }
 }
